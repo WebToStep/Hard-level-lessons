0bf6d5f60882732d1311eb407fbb28784181e4bd
WebStep <webtostep@yandex.kz>
2021-07-31 17:52:41 +0500

    x1z

---
+++ main.js
@@ -0,0 +1,29 @@
+const money = 30000,
+income = 'фриланс',
+addExpenses = 'интернет, такси, коммуналка',
+deposit = true, 
+mission = 2200000, 
+period = 12,
+budgetDay = money/30;
+
+// console.log('money: ', typeof money);
+// console.log('income: ', typeof income);
+// console.log('deposit: ', typeof deposit);
+
+foo({money});
+foo({income});
+foo({deposit});
+function foo(value){
+   console.log(Object.keys(value).toString() + ':', 
+   typeof Object.values(value)[0]);
+
+}
+
+console.log('addExpenses: ', addExpenses.length);
+
+console.log('“Период равен ' + period + ' месяцев”');
+console.log('“Цель заработать ' + mission + ' рублей/долларов/гривен/юани”');
+
+console.log('addExpenses: ', addExpenses.toLocaleLowerCase().split(','));
+
+console.log('budgetDay: ', budgetDay);
