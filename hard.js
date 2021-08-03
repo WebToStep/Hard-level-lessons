20ce3cade7a95fa61a1db966a26e94cca40b643d
WebStep <webtostep@yandex.kz>
2021-08-03 17:58:14 +0500

    L-03-h

--- hard.js
+++ hard.js
@@ -8,12 +8,37 @@ for(let i = 1; i < numArr.length; i++){
    count *= +numArr[i];
 }
 let degree = count ** 3;
-console.log('Умноженый массив: ', count);
-console.log('Итоговое число в степени 3: ', degree);
+// console.log('Умноженый массив: ', count);
+// console.log('Итоговое число в степени 3: ', degree);
 // method 1
-console.log('Первые 2 цифры полученного числа: ', degree.toString().slice(0, 2));
+// console.log('Первые 2 цифры полученного числа: ', degree.toString().slice(0, 2));
 // method 2
 // console.log('Первые 2 цифры полученного числа: ', degree.toString()[0] + degree.toString()[1]);
 
 
+let lang = 'ru';
+let weaks = [];
+weaks['ru'] = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+weaks['en'] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
+
+if(lang === 'ru'){
+   console.log(weaks.ru);
+}else if(lang === 'en'){
+   console.log(weaks.en);
+}else{
+   console.log('Выберите язык');
+}
+
+switch(lang){
+   case 'ru': 
+   console.log(weaks.ru);
+   break;
+   case 'en': 
+   console.log(weaks.en);
+   break;
+   default:
+      console.log('Выберите язык');
+   }
+   
+   console.log(weaks[lang]);
