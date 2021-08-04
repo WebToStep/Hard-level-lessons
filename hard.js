eac28052e08b9d54e905ae692a34d4c39a3f1d82
WebStep <webtostep@yandex.kz>
2021-08-05 01:37:41 +0500

    h-l-3 start

--- hard.js
+++ hard.js
@@ -1,17 +1,2 @@
 'use strict';
 
-const foo = (e)=>{
-   if(typeof e !== 'string'){
-      alert('Введите строку!');
-   }else{
-      e = e.replace( /\s/g, ""); 
-
-      if (e.length > 30){
-         e = e.substring(0, 30) + '(...)';
-         console.log(e);
-      }
-   }
-};
-
-
-foo('1234567890 1234567890 1234567890 0000100023eeeeee');
