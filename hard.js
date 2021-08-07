7dde62315a393923c0489f50c9d01f4c5cbe3cfa
WebStep <webtostep@yandex.kz>
2021-08-07 19:39:44 +0500

    finish

---
+++ hard.js
@@ -0,0 +1,30 @@
+'use strict';
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const day = document.getElementById('fordays'); 
+const todayDay = new Date();
+
+// console.log('ew Date(): ', new Date().getDay());
+
+
+
+
+
+const days = () => {
+   week.forEach((item, i) => {
+      let newdiv = document.createElement('div'); 
+      if (i === +todayDay.getDay() - 1) {
+         newdiv.classList.add('today'); 
+         newdiv.textContent = week[i]; 
+      }
+      if (item == 'Суббота' || item == 'Воскресенье') { 
+         newdiv.classList.add('italic'); 
+         newdiv.textContent = week[i]; 
+      } else {
+         newdiv.textContent = week[i]; 
+      }
+      day.appendChild(newdiv); 
+
+   });
+};
+days(); 
+
