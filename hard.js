1a7e721889ebfaa3fa076a90ec9629fedcc4d3ef
WebStep <webtostep@yandex.kz>
2021-08-07 23:15:11 +0500

    revisions

--- hard.js
+++ hard.js
@@ -1,30 +1,31 @@
 'use strict';
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const day = document.getElementById('fordays'); 
 const todayDay = new Date();
-
-// console.log('ew Date(): ', new Date().getDay());
-
-
-
 
 
 const days = () => {
    week.forEach((item, i) => {
       let newdiv = document.createElement('div'); 
-      if (i === +todayDay.getDay() - 1) {
-         newdiv.classList.add('today'); 
-         newdiv.textContent = week[i]; 
+
+      if (i === +todayDay.getDay() - 1) { 
+         newdiv.style.color = '#d4d010';
+         newdiv.style.paddingLeft = '10px';
+         newdiv.style.fontWeight = '700';
+         newdiv.textContent = "week[i]"; 
       }
-      if (item == 'Суббота' || item == 'Воскресенье') { 
-         newdiv.classList.add('italic'); 
+      if (item === 'Суббота' || item === 'Воскресенье') { 
+         newdiv.style.fontFamily = 'Roboto';
+         newdiv.style.margin = '10px';
+         newdiv.style.fontStyle = 'italic';
          newdiv.textContent = week[i]; 
       } else {
+         newdiv.style.fontFamily = 'Roboto';
+         newdiv.style.margin = '10px';
+         newdiv.style.fontWeight = '100';
          newdiv.textContent = week[i]; 
       }
-      day.appendChild(newdiv); 
-
+      document.body.appendChild(newdiv); 
    });
 };
-days(); 
+days(); // Вызываем функцию;
 
