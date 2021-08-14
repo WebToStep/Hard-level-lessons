2d2a28ea06f8cbe3fdfe7f1482a0e3c2e35afcaf
WebStep <webtostep@yandex.kz>
2021-08-14 18:11:32 +0500

    hard-les13

--- hard.js
+++ hard.js
@@ -1,81 +1,66 @@
 'use strict';
-//стили и элементы страницы
-const body = document.querySelector('body');
+
+let title = document.createElement('h1');
+let btn = document.createElement('button');
+let body = document.body;
+
+title.style.cssText = `
+display: inline-block;
+text-shadow:
+  1px 1px 1px white,
+  1px -1px 1px white,
+  -1px 1px 1px white,
+  -1px -1px 1px white;
+color: steelblue;
+transition: all 1s;
+background-color: #666666;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    background-clip: text;
+    color: transparent;
+    text-shadow: rgba(255,255,255,0.5) 0px 3px 3px;
+    font-size:64px;
+`;
 body.style.cssText = `
-background: linear-gradient(to right, #0f2027, #203a43, #2c5364);
-color: #f2f3f4;
+margin-top:20%;
 display: flex;
+flex-direction: column;
+flex-wrap: wrap;
+align-content: center;
+align-items: center;
 justify-content: center;
+transition: all 0.3s ease-in;
+`;
+btn.style.cssText = `
+background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 40px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 8px;
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
 `;
 
-const main = document.createElement('div');
-main.style.cssText = `
-position: absolute;
-top:45%;
-left:auto;
-text-align:center;
-`;
+body.append(title);
+body.append(btn);
 
-const longTime = document.createElement('h1');
-const shortTime = document.createElement('h2');
+getRandomColor();
+btn.innerText = 'СМЕНИТЬ ЦВЕТ';
 
-body.append(main);
-main.append(longTime);
-main.append(shortTime);
-
-
-
-function Clock() {
-   let todayDay = new Date();
-   let date = todayDay.toLocaleString('ru', {
-      weekday: 'long',
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
-   });
-   date = date.charAt(0).toUpperCase() + date.slice(1, -2);
-
-   const timesString = {
-      h: [' час ', ' часов ', ' часа '],
-      m: [' минута ', ' минут ', ' минуты '],
-      s: [' секунда ', ' секунд ', ' секунды ']
-   };
-
-   let hour = todayDay.getHours();
-   let minutes = todayDay.getMinutes();
-   let seconds = todayDay.getSeconds();
-
-   function textCorrect(value, arr) {
-      let temp = value;
-      if (value > 24) {
-         temp = '' + temp;
-         temp = +temp.charAt(1);
-      }
-      if (+temp === 1 || +temp === 21) {
-         return value += arr[0];
-      } else if (+temp > 4 && +temp < 21 || +temp === 0 || +temp > 24) {
-         return value += arr[1];
-      } else {
-         return value += arr[2];
-      }
+function getRandomColor() {
+   const letters = '0123456789ABCDEF';
+   let color = '#';
+   for (var i = 0; i < 6; i++) {
+     color += letters[Math.floor(Math.random() * 16)];
    }
-
-
-   let dateH1 = 'Сегодня ' + date + 'года, ' +
-      textCorrect(hour, timesString.h) + textCorrect(minutes, timesString.m) + textCorrect(seconds, timesString.s);
-
-   let dateH2 = ("0" + todayDay.getDate()).slice(-2) + "." +
-      ("0" + (todayDay.getMonth() + 1)).slice(-2) + "." +
-      todayDay.getFullYear() + " - " +
-      ("0" + todayDay.getHours()).slice(-2) + ":" +
-      ("0" + todayDay.getMinutes()).slice(-2) + ":" +
-      ("0" + todayDay.getSeconds()).slice(-2);
-
-
-   longTime.innerText = dateH1;
-   shortTime.textContent = dateH2;
-
-   setInterval(() => Clock(), 1000);
-}
-
-Clock();
+   console.log(color);
+   title.innerText = color;
+   body.style.background = color;
+   return color;
+ }
+ 
+ btn.addEventListener('click', getRandomColor);
+ 
